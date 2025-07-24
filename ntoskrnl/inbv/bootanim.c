f82190798187f5a172afe4c24d962ab7ce026dce
NearestCommit <nearestcommit@gmail.com>
2025-07-24 13:54:08 -0500

    InbvDrawText
    cleanup and beginning of NtDrawText impl
    
    Implemented NtDrawText
    
    cleanup
    
    a bit more cleanup
    
    Implemented NtDrawText and InbvDrawText

--- ntoskrnl/inbv/bootanim.c
+++ ntoskrnl/inbv/bootanim.c
@@ -803,13 +803,13 @@ FinalizeBootLogo(VOID)
     if (InbvGetDisplayState() == INBV_DISPLAY_STATE_OWNED)
     {
         /* Clear the screen */
-        VidSolidColorFill(0, 0, SCREEN_WIDTH-1, SCREEN_HEIGHT-1, BV_COLOR_BLACK);
+        // VidSolidColorFill(0, 0, SCREEN_WIDTH-1, SCREEN_HEIGHT-1, BV_COLOR_BLACK);
     }
 
     /* Reset progress bar and lock */
 #ifdef INBV_ROTBAR_IMPLEMENTED
-    PltRotBarStatus = RBS_STOP_ANIMATE;
-    RotBarThreadActive = FALSE;
+    // PltRotBarStatus = RBS_STOP_ANIMATE;
+    // RotBarThreadActive = FALSE;
 #endif
     InbvReleaseLock();
 }
